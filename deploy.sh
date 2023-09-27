dd56e54e32e0ce09d02b2f803ab8bc72e9ead1f9
tonz <mcteamster@gmail.com>
2023-09-27 20:11:09 +1000

    cache resources

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 aws --region us-east-1 cloudformation deploy --stack-name libra --template-file ./ci/template.yaml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --parameter-overrides DomainName=tonz.io SubDomain=libra HostedZoneId=Z0647044GG15YFJEKM CreateApex=no
-aws s3 sync "./dist" "s3://libra-customresourcestack-tn8w1bls2f-s3bucketroot-5saolti5l3l2" --cache-control no-cache
+aws s3 sync "./dist" "s3://libra-customresourcestack-tn8w1bls2f-s3bucketroot-5saolti5l3l2"
+aws cloudfront create-invalidation --distribution-id="EROO7XKKI363S" --paths="/*"
