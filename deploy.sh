63fef768b4f1f347cc5798537af2ae465b8c3e2a
tonz <mcteamster@gmail.com>
2024-02-27 01:56:37 +1100

    update version

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 aws --region us-east-1 cloudformation deploy --stack-name libra --template-file ./ci/template.yaml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --parameter-overrides DomainName=tonz.io SubDomain=libra HostedZoneId=Z0647044GG15YFJEKM CreateApex=no
+npm ci
+npm run build
 aws s3 sync "./dist" "s3://libra-customresourcestack-tn8w1bls2f-s3bucketroot-5saolti5l3l2"
 aws cloudfront create-invalidation --distribution-id="EROO7XKKI363S" --paths="/*"
